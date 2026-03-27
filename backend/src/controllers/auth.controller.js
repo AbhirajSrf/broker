256c228ea8ef9f83200f299e8ed8fa7cadfa66c7
Abhiraj Rai <raiayutra@gmail.com>
2026-03-27 16:16:36 +0545

    login and signup completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,79 @@
-export const signup = async (req, res) => {};
-export const login = async (req, res) => {};
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req, res) => {
+  const { fullName, userName, email, password } = req.body;
+
+  try {
+    if (!fullName || !userName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const existingUser = await User.findOne({ $or: [{ userName }, { email }] });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Username or email already taken" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      userName,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+    generateToken(savedUser._id, res);
+
+    res.status(201).json({
+      _id: savedUser._id,
+      fullName: savedUser.fullName,
+      userName: savedUser.userName,
+      email: savedUser.email,
+    });
+  } catch (error) {
+    console.error("Error in signup controller: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { userName, password } = req.body;
+
+  if (!userName || !password) {
+    return res
+      .status(400)
+      .json({ message: "Both user name and password are required" });
+  }
+
+  try {
+    const user = await User.findOne({ userName });
+    if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      userName: user.userName,
+    });
+  } catch (error) {
+    console.error("Error in login controller: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 export const logout = async (req, res) => {};
